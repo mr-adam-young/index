cefb4ac346afbf02bd69ff0c0980245d778f75f6
Adam Young <adam.young@interversal.systems>
2021-07-19 21:07:50 -0400

    some changes to the inspector

--- index_inspector.php
+++ index_inspector.php
@@ -1,16 +1,25 @@
-<?php
-/* 
-
+<?php /* -------------------------------------
 # index_inspector.php
-For loading a single node as a webpage
 
-*/
+Load a single node as a webpage and display its properties and relationships
+---------------------------------------------*/
 require_once __DIR__.'/includes/main-include.php';
 require_once __DIR__.'/includes/panel-header.php';
 ?>
 
 <pre>
-<?php print_r(interverse_neo4j("MATCH (n) WHERE ID(n) = {$_GET['id']} RETURN n", false)); ?>
+<?php 
+
+$result = interverse_neo4j("MATCH (n) WHERE ID(n) = {$_GET['id']} RETURN n", false); 
+echo "<p>Raw result</p>";
+
+$result = get_object_vars($result);
+echo "<p>get_object_vars()</p>";
+var_dump($result);
+echo "<p>json encode+decode trick</p>";
+echo json_decode(json_encode($result));
+
+?>
 </pre>
 
 <?php
