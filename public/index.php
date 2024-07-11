46a390871f4c5bd31e8606107236a62812da4d48
younga <adam.builds@outlook.com>
2024-07-11 01:07:01 -0400

    Merge with a fresh Laravel installation

--- public/index.php
+++ public/index.php
@@ -1,25 +1,17 @@
 <?php
 
-require 'includes/main-include.php';
-require 'includes/panel-header.php';
+use Illuminate\Http\Request;
 
-if (isset($_GET['job'])) {
-	require 'views/job.php';
-} elseif (isset($_GET['edit'])) {
-	require 'views/job-edit.php';
-} elseif (isset($_GET['search'])) {
-  $title = "Search: ".$_GET['search'];
-  $list = db("SELECT * FROM Jobs WHERE Description LIKE '%".$_GET['search']."%' OR Title LIKE '%".$_GET['search']."%' ORDER BY ID DESC");
-  require 'views/job-list.php';
-} else {
-if (!isset($_GET['all'])) {
-    $title = "Active Jobs";
-    $list = db("SELECT * FROM Jobs INNER JOIN StatusCodes ON Jobs.Status=StatusCodes.Value WHERE Status > 0 AND Status < 100 ORDER BY Status ASC");
-  } else {
-    $title = "All Jobs Ever";
-    $list = db("SELECT * FROM Jobs INNER JOIN StatusCodes ON Jobs.Status=StatusCodes.Value ORDER BY ID DESC");
-	}
-  require 'views/job-list.php';
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
-require 'includes/panel-footer.php'; 
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
