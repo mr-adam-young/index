93e4fb5092255bbe6a11e2d25ac43c4f8d02ac0c
younga <adam.builds@outlook.com>
2024-08-13 12:37:28 -0400

    Nice logo

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+dd('API routes loaded');
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\LaborController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/clockshark', [LaborController::class, 'handle']);
+Route::get('/test', function () {
+    return response()->json(['message' => 'Test route']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
