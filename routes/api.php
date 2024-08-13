fb3b7c8c85b5b5c8affc36f3927312098c568c96
Adam Young <33268910+adambuilds@users.noreply.github.com>
2024-08-13 10:12:33 -0400

    Controller and route to receive clocks

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/clockshark', [App\Http\Controllers\LaborController::class, 'logPostData']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
